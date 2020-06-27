70c61582e9ade7460dfbf9fdcc16aa22891dbba5
Andy Cockerill <cockerill.andrew@gmail.com>
2020-06-27 20:41:52 +0100

    Incremental file naming

--- spypy_test.py
+++ spypy_test.py
@@ -11,8 +11,23 @@ GPIO.setmode(GPIO.BCM)
 PIR = 4
 GPIO.setup(PIR, GPIO.IN)
 
+def getNextFilePath(output_folder):
+    highest_num = 0
+    for f in os.listdir(output_folder):
+        if os.path.isfile(os.path.join(output_folder, f)):
+            file_name = os.path.splitext(f)[0]
+            try:
+                file_num = int(file_name)
+                if file_num > highest_num:
+                    highest_num = file_num
+            except ValueError:
+                'The file name "%s" is not an integer. Skipping' % file_name
+
+    output_file = os.path.join(output_folder, str(highest_num + 1))
+    return output_file
+
 camera = PiCamera()
 camera.resolution = (1024, 768)
 
 sleep(2)
-camera.capture('foo.jpg')
+camera.capture(getNextFilePath)
