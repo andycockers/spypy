490b59476692fac26f02f47e99c01594b9d222db
Andy Cockerill <cockerill.andrew@gmail.com>
2020-06-28 16:24:32 +0100

    New test file

---
+++ spypy_test_2.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+import subprocess
+import os
+from configparser import ConfigParser
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+PIR = 4
+GPIO.setup(PIR, GPIO.IN)
+
+def wait():
+    GPIO.input(PIR)
+
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    wait()
+    for filename in camera.capture_continuous('img{timestamp:%Y-%m-%d-%H-%M}.jpg'):
+        print('Captured %s' % filename)
+        wait()
