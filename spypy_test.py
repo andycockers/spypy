ad4f7e7877f82d630d37a3f5458fa723b8f660cc
Andy Cockerill <cockerill.andrew@gmail.com>
2020-06-29 10:57:36 +0100

    Change

--- spypy_test.py
+++ spypy_test.py
@@ -26,10 +26,8 @@ try:
                print("Ready")
                while True:
                              if GPIO.input(PIR):
-                                             print("Motion detected, capturing image")
-                                             time.sleep(1)
-                                             camera.capture_continuous(filename)
-                                             time.sleep(1)
+                                            for filename in camera.capture_continuous('img{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+                                             print('Captured %s' % filename)
                              time.sleep(1)
 except KeyboardInterrupt:
                print(" Quit")
