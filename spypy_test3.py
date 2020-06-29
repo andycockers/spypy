d966d8a9e6614949edfcf476e9b9abd33b9141b2
Andy Cockerill <34304883+andycockers@users.noreply.github.com>
2020-06-29 13:34:13 +0100

    Update spypy_test3.py

--- spypy_test3.py
+++ spypy_test3.py
@@ -10,11 +10,11 @@ counter = 1
 
 while True:
   if GPIO.input (pirPin) == GPIO.LOW:
-  try: 
+   try: 
     camera.start_preview()
     time.sleep(1) 
     camera.capture('/home/pi/image%s.jpg' % counter counter = counter + 1)
     camera.stop_preview()
-  except:
+   except:
     camera.stop_preview()
     time.sleep(3)
