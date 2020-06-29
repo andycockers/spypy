fd005cca72ee3e76a9e8923ec54893c2d5529436
Andy Cockerill <34304883+andycockers@users.noreply.github.com>
2020-06-29 13:27:54 +0100

    Update spypy_test3.py

--- spypy_test3.py
+++ spypy_test3.py
@@ -8,6 +8,11 @@ setmode (GPIO.BCM)
 :
   if GPIO
   .input (pirPin) == GPIO.LOW:
-  try: camera.start_preview ()time.sleep (1) camera.capture ('/home/pi/image%s.jpg' % counter) counter = counter + 1 camera.stop_preview ()except:camera.stop_preview ()time.
-      sleep
-      (3)
+  try: 
+    camera.start_preview()
+    time.sleep(1) 
+    camera.capture('/home/pi/image%s.jpg' % counter counter = counter + 1)
+    camera.stop_preview()
+  except:
+    camera.stop_preview()
+    time.sleep(3)
